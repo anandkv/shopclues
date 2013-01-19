27176b8e9d9b26180dcf93e29bf32e7947c64a95
Brain Box <techbyte101@gmail.com>
2013-01-19 17:01:13 +0530

    Updated for Sync

--- Development/Code/PHP/MilkRun/api/json/pod.php
+++ Development/Code/PHP/MilkRun/api/json/pod.php
@@ -1,8 +1,6 @@
 <?php
 require("../common/db.php");
 
-require_once dirname(__FILE__).'/../../../../../prepare.php';
-require_once dirname(__FILE__).'/../../../../../init.php';
 $action = $_REQUEST['action'];
 $timeFomat = "%Y-%m-%d %H:%m:%s";
 
@@ -17,7 +15,6 @@ $searchCriteria['mName'] =  $_REQUEST["mName"];
 $searchCriteria['manifestId'] =  $_REQUEST["manifestId"];
 $searchCriteria['pbName'] =  $_REQUEST["pbName"];
 $searchCriteria['toDate']	=  $_REQUEST["toDate"];
-
 $toDate = strtotime ($searchCriteria['toDate']);
 $fromDate = strtotime ($searchCriteria['fromDate']);
 
@@ -68,10 +65,22 @@ function podSummary(){
 	$manifestId =  $_REQUEST["manifestId"];
 	$pbName =  $_REQUEST["pbName"];
 
-	$query="SELECT mf.pickupboy_id ,pb.pickupboy, mf.manifest_id,FROM_UNIXTIME(mf.logout_date, '$timeFomat') logout_dt, FROM_UNIXTIME(mf.login_date, '$timeFomat') login_dt, FROM_UNIXTIME(dispatch_date, '%d-%m-%Y') AS date, SUM( received_qty ) received_qty, SUM( expected_qty ) expected_qty
-			FROM clues_mri_manifest_details_mobile_app mfd
-			JOIN clues_mri_manifest mf ON mfd.manifest_id = mf.manifest_id
-			JOIN clues_pickupboy pb ON mfd.pickupboy_id = pb.pickupboy_id
+	$query="SELECT mf.pickupboy_id ,pb.pickupboy, mf.manifest_id,
+			case mf.logout_date
+			when null then ''
+			when 0 then ''
+			else FROM_UNIXTIME(mf.logout_date, '%Y-%m-%d %H:%m:%s')
+			END  logout_dt, 
+			case mf.login_date
+			when null then ''
+			when 0 then ''
+			else FROM_UNIXTIME(mf.login_date, '%Y-%m-%d %H:%m:%s')
+			END  login_dt,
+			FROM_UNIXTIME(dispatch_date, '%d-%m-%Y') AS date, SUM( received_qty ) received_qty, SUM( expected_qty ) expected_qty
+						FROM clues_mri_manifest_details_mobile_app mfd
+						JOIN clues_mri_manifest mf ON mfd.manifest_id = mf.manifest_id
+						JOIN clues_pickupboy pb ON mfd.pickupboy_id = pb.pickupboy_id
+
 
 			where (mf.manifest_id = '$manifestId' or '$manifestId' = '')
 			and (mfd.pickupboy_id = '$pbName' or '$pbName' = '')
@@ -110,9 +119,22 @@ function listProducts(){
 function listMerchants(){
 	global $searchCriteria, $fromDate, $toDate, $timeFomat;
 	$pid = $searchCriteria['pid'];
-	$query="select merchant_id, FROM_UNIXTIME(mfd.close_date, '$timeFomat' ) as close_dt, company_name, status, company_add, sum(expected_qty) expected_qty, sum(received_qty) received_qty  from
-			clues_mri_manifest_details_mobile_app mfd 
-			left outer join  clues_mri_manifest mf ON mfd.manifest_id = mf.manifest_id
+	$query="select 
+    merchant_id,
+    case mfd.close_date
+        when null then ''
+        when 0 then ''
+        else FROM_UNIXTIME(mfd.close_date, '%Y-%m-%d %H:%m:%s')
+    END close_dt,
+    company_name,
+    status,
+    company_add,
+    sum(expected_qty) expected_qty,
+    sum(received_qty) received_qty
+from
+    clues_mri_manifest_details_mobile_app mfd
+        left outer join
+    clues_mri_manifest mf ON mfd.manifest_id = mf.manifest_id
 			where mfd.pickupboy_id = '$pid'
 			and mfd.company_name like '%".$searchCriteria[mName]."%'
 			and mfd.company_add like '%".$searchCriteria[mAddress]."%' 
