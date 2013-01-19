9ee1eed30d9aa8084d35e9706aa3ff88841a04d0
Pramod Bindal <bindal.pramod@gmail.com>
2013-01-20 00:28:05 +0530

    ***

--- Development/Code/PHP/MilkRun/api/common/queries.sql
+++ Development/Code/PHP/MilkRun/api/common/queries.sql
@@ -6,13 +6,8 @@ select
     max(download_date) download_date,
     max(dispatch_date) dispatch_date
 from
-    (select 
-        *
-    from
-        clues_pickupboy
-    where
-        imei is not null and imei != '') pb
-        left outer join
+    (select  * from clues_pickupboy where imei is not null and imei != '') pb
+    left outer join
     (SELECT 
         manifest_id,
             company_id,
@@ -27,4 +22,4 @@ from
         clues_mri_manifest mf
 
 ) manifest ON pb.pickupboy_id = manifest.pickupboy_id
-group by manifest_id , company_id , pickupboy_id
+group by manifest_id , company_id , pickupboy_id;
