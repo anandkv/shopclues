e8ec9dfd8a772eced2418da9dfce55d4f4a9a07a
Pramod Bindal <bindal.pramod@gmail.com>
2013-01-19 23:08:43 +0530

    clues_mri_manifest modified

--- Development/Code/SQL/Tables/clues_mri_manifest.sql
+++ Development/Code/SQL/Tables/clues_mri_manifest.sql
@@ -1,9 +1,9 @@
 CREATE TABLE clues_mri_manifest ( 
-id bigint( 20 ) NOT NULL auto_increment , 
+	id bigint( 20 ) NOT NULL auto_increment , 
 	manifest_id bigint( 20 ) default NULL , 
-	company_id( 20 ) default NULL , 
+	company_id bigint( 20 ) default NULL , 
 	pickupboy_id bigint( 20 ) default NULL , 
-	status varchar(32) default nul
+	status varchar(32) default null,
 	status_date int( 11 ) default NULL,
 	created_date int( 11 ) default NULL, 
 	created_by varchar(255) default NULL, 
@@ -11,5 +11,4 @@ id bigint( 20 ) NOT NULL auto_increment ,
 	updated_by varchar(255) default NULL, 
 PRIMARY KEY ( id ) , 
 UNIQUE KEY manifest_id ( manifest_id,company_id) 
-
 ) ENGINE = InnoDB ; 
